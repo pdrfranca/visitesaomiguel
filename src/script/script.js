9084410157bfb84f1be5f13cf87bc156e265d0fd
Pedro França <pfrutopro@gmail.com>
2024-09-24 10:15:29 -0300

    Add files via upload

---
+++ src/script/script.js
@@ -0,0 +1,16 @@
+// Funções de acessibilidade
+
+// Aumentar fonte
+function aumentarFonte() {
+    document.body.style.fontSize = '18px';
+}
+
+// Diminuir fonte
+function diminuirFonte() {
+    document.body.style.fontSize = '14px';
+}
+
+// Alternar contraste
+function toggleContraste() {
+    document.body.classList.toggle('alto-contraste');
+}
